f34175b61fc34707b6015c5a5aac7b2421c23a60
Jonas <57275206+lackblack@users.noreply.github.com>
2023-12-18 11:29:33 +0200

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function findPerson() {
   const birthdate = document.getElementById('birthdate').value;
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   const apiUrl = `https://en.wikipedia.org/w/api.php?action=query&list=peoplewhothisdatepassedaway&pwtdpadate=${birthdate}&format=json`;
 
-  fetch(apiUrl)
+  fetch(proxyUrl + apiUrl)
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok.');
@@ -11,13 +12,15 @@ function findPerson() {
     })
     .then(data => {
       // Handle the response data
-      console.log(data); // Log the retrieved data to inspect in the browser console
+      console.log(data);
+      // Display the data to the user or perform further actions
     })
     .catch(error => {
       console.error('Error:', error);
       document.getElementById('result').innerHTML = "An error occurred while fetching data. Please check the console for more details.";
     });
 }
+
  else {
         document.getElementById('result').innerHTML = "No historical figures found for this date.";
       }
