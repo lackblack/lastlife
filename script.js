542f3861a08b53ec2653a2b3e7371ca39ef8d358
Jonas <57275206+lackblack@users.noreply.github.com>
2023-12-18 12:55:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,14 @@ function findDeathDate() {
         }
 
         if (deathsByYear[selectedYear] && deathsByYear[selectedYear].length > 0) {
-          const resultText = `<b>${selectedYear}:</b><br>${deathsByYear[selectedYear].map(death => death.text).join('<br>')}`;
+          const resultText = `<b>${selectedYear}:</b><br>${deathsByYear[selectedYear].map(death => {
+            let imageUrl = '';
+            if (death.pages && death.pages[0]) {
+              const pageTitle = death.pages[0].title.replace(/ /g, '_');
+              imageUrl = `https://en.wikipedia.org/api/rest_v1/page/summary/${pageTitle}`;
+            }
+            return `<div><img src="${imageUrl}" width="100" height="100">${death.text}</div>`;
+          }).join('<br>')}`;
           document.getElementById('lastLife').innerHTML = resultText;
         } else {
           document.getElementById('lastLife').textContent = 'No recorded deaths on your birth year or the closest year available.';
