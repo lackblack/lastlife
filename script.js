cf2a905703fbbfbaeca7e1ea1959ab1d7ab34fe9
Jonas <57275206+lackblack@users.noreply.github.com>
2023-12-18 12:21:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-function getNewArticle() {
-  fetch('https://en.wikipedia.org/api/rest_v1/page/random/summary')
+function findDeathDate() {
+  const birthdate = document.getElementById('birthdate').value;
+  const formattedDate = new Date(birthdate);
+  const month = formattedDate.getMonth() + 1;
+  const day = formattedDate.getDate();
+
+  fetch(`https://en.wikipedia.org/api/rest_v1/feed/onthisday/deaths/${month}/${day}`)
     .then(response => response.json())
     .then(data => {
-      const articleContent = document.getElementById('article-content');
-      articleContent.textContent = data.extract;
+      if (data.deaths && data.deaths.length > 0) {
+        const randomIndex = Math.floor(Math.random() * data.deaths.length);
+        const lastLifeInfo = data.deaths[randomIndex];
+        const lastLifeText = `On your birthday, someone like you passed away: ${lastLifeInfo.text}`;
+        document.getElementById('lastLife').textContent = lastLifeText;
+      } else {
+        document.getElementById('lastLife').textContent = 'No matching death found on that day.';
+      }
     })
     .catch(error => console.log(error));
-}
-
-function checkGuess() {
-  const userGuess = document.getElementById('user-guess').value.toLowerCase();
-  const articleContent = document.getElementById('article-content').textContent.toLowerCase();
-
-  const result = document.getElementById('result');
-  if (articleContent.includes(userGuess)) {
-    result.textContent = 'Correct! You guessed it right.';
-  } else {
-    result.textContent = 'Oops! Try again.';
-  }
 }
